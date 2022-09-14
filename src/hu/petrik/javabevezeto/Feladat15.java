01776741690fd366c932931caf657e0533da58d7
Fuzi18 <112927613+Fuzi18@users.noreply.github.com>
2022-09-14 20:10:56 +0200

    Add files via upload

---
+++ src/hu/petrik/javabevezeto/Feladat15.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Feladat15 {
+    public static void main(String[] args) {
+        double [] tomb= new double[25];
+        for (int i = 0; i < tomb.length; i++) {
+            tomb[i]=Math.random()*200-100;
+
+        }
+        for (int i = 0; i < tomb.length; i++) {
+            System.out.print((tomb[i] * tomb[i]) + " ");
+        }
+    }
+}
