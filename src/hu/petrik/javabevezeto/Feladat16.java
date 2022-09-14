fa8150f0efba128cbb9f68cbfeedfbb79af8fb59
Fuzi18 <112927613+Fuzi18@users.noreply.github.com>
2022-09-14 20:36:49 +0200

    Add files via upload

---
+++ src/hu/petrik/javabevezeto/Feladat16.java
@@ -0,0 +1,14 @@
+public class Feladat16 {
+    public static void main(String[] args) {
+        System.out.println("Kérem adjon meg az 1. számot: ");
+        System.out.println("Kérem adjon meg az 2. számot: ");
+        System.out.println("Kérem adjon meg az 3. számot: ");
+        System.out.println("Kérem adjon meg az 4. számot: ");
+        System.out.println("Kérem adjon meg az 5. számot: ");
+        double[] tomb=new double[5];
+        for (int i = 0; i < tomb.length; i++) {
+            
+        }
+
+    }
+}
